678bbba88a6e6aa8ea97977105c9b2ab8d2d88b9
FXKevinK <58078219+FXKevinK@users.noreply.github.com>
2020-02-27 21:13:06 +0700

    Update soal2.sh

--- soal2/soal2.sh
+++ soal2/soal2.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
-randompswd=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 28 ; echo '')
+randompswd1=$(head /dev/urandom | tr -dc A-Z | head -c 1 ; echo '')
+randompswd2=$(head /dev/urandom | tr -dc a-z | head -c 1 ; echo '')
+randompswd3=$(head /dev/urandom | tr -dc 0-9 | head -c 1 ; echo '')
+randompswd4=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 25 ; echo '')
+
+
+case "$((RANDOM % 6))" in
+  "0")
+  randompswd="$randompswd1$randompswd2$randompswd3$randompswd4"
+  ;;
+  "1")
+  randompswd="$randompswd1$randompswd3$randompswd2$randompswd4"
+  ;;
+  "2")
+  randompswd="$randompswd3$randompswd2$randompswd1$randompswd4"
+  ;;
+  "3")
+  randompswd="$randompswd3$randompswd1$randompswd2$randompswd4"
+  ;;
+  "4")
+  randompswd="$randompswd2$randompswd3$randompswd1$randompswd4"
+  ;;
+  "5")
+  randompswd="$randompswd2$randompswd1$randompswd3$randompswd4"
+  ;;
+esac
+
+
 echo "kode unik tercipta : $randompswd"
 
-NamaFileAwal=${1%.*}
+NamaFileAwal=${1%%.*}
 
 if [[ $NamaFileAwal =~ [0-9] ]]
   then
